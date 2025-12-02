bf2a6c1de18441a1b8106b7644bbbe0eef654088
Yuvaraj Tuli <79062386+Yuvarajtuli@users.noreply.github.com>
2025-12-02 16:59:18 +0530

    Refactor buildDriver function in driver.js
    Removed unnecessary line for chromedriver path setting.

--- tests/base/driver.js
+++ tests/base/driver.js
@@ -4,16 +4,15 @@ import chrome from "selenium-webdriver/chrome.js";
 export default async function buildDriver() {
   const options = new chrome.Options();
 
-  // Required for CI (GitHub Actions)
+  // Required for GitHub Actions
   options.addArguments("--headless=new");
-  options.addArguments("--no-sandbox");
   options.addArguments("--disable-gpu");
+  options.addArguments("--no-sandbox");
   options.addArguments("--disable-dev-shm-usage");
 
-  const driver = await new Builder()
+  return await new Builder()
     .forBrowser("chrome")
     .setChromeOptions(options)
+    // DO NOT SET chromedriver path – use system driver
     .build();
-
-  return driver;
 }
